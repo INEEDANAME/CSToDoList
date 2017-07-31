7e052e772ace1be862d7874425afc73c41a734c6
arshdeep <arshdeepsdhanjal@gmail.com>
2017-07-31 18:19:08 -0400

    Added User interface Improvements

---
+++ CSToDoList/CreateListForm.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace CSToDoList
+{
+    partial class CreateListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnCreateList = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "List Name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(123, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(317, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(34, 104);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(406, 142);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Options";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Category :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Student",
+            "Co-Worker",
+            "Teacher",
+            "Doctor"});
+            this.comboBox1.Location = new System.Drawing.Point(131, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(161, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email : ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(131, 82);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(161, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // btnCreateList
+            // 
+            this.btnCreateList.Location = new System.Drawing.Point(351, 272);
+            this.btnCreateList.Name = "btnCreateList";
+            this.btnCreateList.Size = new System.Drawing.Size(89, 30);
+            this.btnCreateList.TabIndex = 3;
+            this.btnCreateList.Text = "Create";
+            this.btnCreateList.UseVisualStyleBackColor = true;
+            // 
+            // CreateListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 340);
+            this.Controls.Add(this.btnCreateList);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "CreateListForm";
+            this.Text = "CreateListForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCreateList;
+    }
+}
--- CSToDoList/Form1.cs
+++ CSToDoList/Form1.cs
@@ -16,5 +16,44 @@ namespace CSToDoList
         {
             InitializeComponent();
         }
+
+        private void lbToDoList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtbToDoInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddTodo_Click(object sender, EventArgs e)
+        {
+               lbToDoList.Items.Add(txtbToDoInput.Text);
+            
+        }
+
+        private void btnNewList_Click(object sender, EventArgs e)
+        {
+            CreateListForm frm = new CreateListForm();
+            frm.Show();
+        }
+
+        private void btnShowSettings_Click(object sender, EventArgs e)
+        {
+            if (baseSplitContainer.SplitterDistance == 63)
+            {
+                baseSplitContainer.SplitterDistance = 240;
+            }
+            else
+            {
+                baseSplitContainer.SplitterDistance = 63;
+            }
+        }
+
+        private void baseSplitContainer_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
